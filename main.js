4da3742206bfede1f748436fd8024b5e3ac48330
unknown <qoravideolarnew@gmail.com>
2023-06-07 12:16:45 +0500

    Homework5

---
+++ main.js
@@ -0,0 +1,44 @@
+module.exports = {
+    bestsellers: [
+        {
+            id: 1,
+            title : 'Bahor gullari',
+        },
+        {
+            id: 2,
+            title : 'Duo taqdirni o`zgartiradi',
+        },
+        {
+            id: 3,
+            title : 'Temur tuzuklari',
+        },
+    ],
+    populars : [
+        {
+            id: 1,
+            title : 'O‘yla va boy bo‘l',
+        },
+        {
+            id: 2,
+            title : 'Da Vinchi siri',
+        },
+        {
+            id: 3,
+            title : "Garri Potter va hikmatlar toshi",
+        },
+    ],
+    historical : [
+        {
+            id: 1,
+            title : 'Amir temur haqida hikoyalar',
+        },
+        {
+            id: 2,
+            title : 'Alchemist',
+        },
+        {
+            id: 3,
+            title : "Sahroda porlagan nur",
+        },
+    ],
+}
